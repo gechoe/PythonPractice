7b2a170e09a9c6fae2659d1fa9e0622960d0ccc9
gechoe <98035309+gechoe@users.noreply.github.com>
2024-07-19 18:31:57 -0400

    Update BudgetList.py

---
+++ BuildPersonalBudgetReport/Module2/BudgetList.py
@@ -0,0 +1,39 @@
+from budget import Expense
+
+# Class that extends list type
+class BudgetList():
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+    
+    def append(self, item):
+        # Checks if item is a number
+        if (self.sum_expenses+item < self.budget):
+            self.expenses.append(item)
+            self.sum_expenses += item
+        else:  # Else appends to the overages list and adds to the overage total
+            self.overages.append(item)
+            self.sum_overages+=item
+
+    def __len__(self):
+        return (len(self.expenses) + len(self.overages))
+
+    
+def main():
+    # Setting budget to 1200
+    myBudgetList = BudgetList(1200)
+  
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+    
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+
+    print('The count of all expenses: ' + str(len(myBudgetList)))
+
+
+if __name__ == "__main__":
+    main()
