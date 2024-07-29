1903885f37f6c59a7b2b4c4e1e9f64f8cafd8004
Grace <98035309+gechoe@users.noreply.github.com>
2024-07-29 10:34:44 -0400

    Update tablesPractice2.sql

--- SQLBasics/tablesPractice2.sql
+++ SQLBasics/tablesPractice2.sql
@@ -1,1 +1,22 @@
+-- From Pluralsight practice: SQL: Creating Tables, Selecting, Inserting, and Deleting Data (Interactive)
+-- Link: https://app.pluralsight.com/library/courses/sql-tables-selecting-inserting-deleting-data/table-of-contents
 
+-- Part 1
+-- Practice 1.1 - Challenge: Fix SELECT
+-- Practice 1.2 - Challenge: Introducing SELECT
+-- Practice 1.3 - Challenge: Returning Title
+-- Practice 1.4 - Challenge: Adding to SELECT
+-- Practice 1.5 - Challenge: Adjusting to SELECT
+
+
+-- Part 2
+-- Practice 2.1 - Challenge: Adding Films
+-- Practice 2.2 - Challenge: Not All Info
+-- Practice 2.3 - Challenge: Updating Genre
+-- Practice 2.4 - Challenge: Multiple Updates
+-- Practice 2.5 - Challenge: Removing Movies
+
+
+-- Part 3
+-- Practice 3.1 - Challenge: Create Advertisements
+-- Practice 3.2 - Challenge: Structured Data
